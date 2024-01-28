694d1efd54cc47992ea55b9a6eef2850635dd007
Yasin <yasin.junet@gmail.com>
2024-01-28 21:16:09 +0700

    feat: tutorial if-else dart

---
+++ bin/if.dart
@@ -0,0 +1,10 @@
+void main() {
+  var nilai = 85;
+  var absen = 59;
+
+  if (nilai >= 75 && absen >= 60) {
+    print('Selamat Anda lulus!');
+  } else {
+    print('Silahkan coba kembali');
+  }
+}
